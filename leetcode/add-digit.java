0f1b95d58bf3081a05a13292636df8af9c69bbf3
monish appusamy <monishappusamy@gmail.com>
2015-11-16 02:01:02 -0500

    Create add-digit.java

---
+++ leetcode/add-digit.java
@@ -0,0 +1,17 @@
+/**
+* Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+*/
+
+public class Solution {
+    public int addDigits(int num) {
+        int i = 0;
+        while(num != 0){
+            i = i + num % 10;
+            num = num / 10;
+        }
+        if(i > 9){
+            i = addDigits(i);
+        }
+        return i;
+    }
+}
