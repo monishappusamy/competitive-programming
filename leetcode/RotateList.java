747040eebb71674b91ebdcfe024b27e1ca00048b
monish appusamy <monishappusamy@gmail.com>
2015-11-28 16:06:09 -0500

    Update RotateList.java

--- leetcode/RotateList.java
+++ leetcode/RotateList.java
@@ -1,6 +1,37 @@
 /**
  * Given 1->2->3->4->5->NULL and k = 2,
  * return 4->5->1->2->3->NULL.
+ * 
+ * Good Solution:
+ * class Solution {
+ * public:
+ *   ListNode* rotateRight(ListNode* head, int k) {
+ *       if (!head) return NULL;
+ *       int len = listLength(head);
+ *       k %= len;
+ *       ListNode* fast = head;
+ *       for (int i = 0; i < k; i++)
+ *           fast = fast -> next;
+ *       ListNode* slow = head;
+ *       while (fast -> next) {
+ *           slow = slow -> next;
+ *           fast = fast -> next; 
+ *       }
+ *       fast -> next = head;
+ *       head = slow -> next;
+ *       slow -> next = NULL; 
+ *       return head;
+ *   }
+ * private:
+ *   int listLength(ListNode* head) {
+ *       int len = 0;
+ *       while (head) {
+ *           len++;
+ *           head = head -> next;
+ *       }
+ *       return len;
+ *   }
+ * };
  */
 
 /**
