4b9e4456264e53bc9ffc9f5292cf2dc6cdb63e39
monish appusamy <monishappusamy@gmail.com>
2015-11-29 15:12:05 -0500

    Update ReverseList.java

--- leetcode/ReverseList.java
+++ leetcode/ReverseList.java
@@ -1,4 +1,20 @@
 /**
+ * Best Solution:
+ * public ListNode reverseList(ListNode head) {
+ *   ListNode prev = null;
+ *   ListNode curr = head;
+ *   while (curr != null) {
+ *       ListNode nextTemp = curr.next;
+ *       curr.next = prev;
+ *       prev = curr;
+ *       curr = nextTemp;
+ *   }
+ *   return prev;
+}
+ * 
+ * /
+
+/**
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
@@ -11,14 +27,13 @@ public class Solution {
     ListNode newList = new ListNode(0); 
     public ListNode reverseList(ListNode head) {
         ListNode current = head;
-        
-		while(current != null){
-			ListNode temp = new ListNode(0);
-			temp.val = current.val;
-			temp.next = newList.next;
-			newList.next = temp;
-			current = current.next;
-		}
+	while(current != null){
+		ListNode temp = new ListNode(0);
+		temp.val = current.val;
+		temp.next = newList.next;
+		newList.next = temp;
+		current = current.next;
+	}
         return newList.next;
     }
 }
