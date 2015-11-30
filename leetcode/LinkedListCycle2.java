306adcf08bb30e7979e07139ce984a629e88d879
monish appusamy <monishappusamy@gmail.com>
2015-11-30 17:33:55 -0500

    Update LinkedListCycle2.java

--- leetcode/LinkedListCycle2.java
+++ leetcode/LinkedListCycle2.java
@@ -22,15 +22,15 @@ public class Solution {
   	    if(fast == null || fast.next == null)
   		    return false;
   	    slow = slow.next;
-        fast = fast.next.next;
-  	    if(slow == fast){
-  	        ListNode slow2 = head;
-			      while(slow != slow2){
-			          slow = slow.next;
-			    	    slow2 = slow2.next;
-			    }
-			    return slow;
-			    }
-    	}
+            fast = fast.next.next;
+	    if(slow == fast){
+	        ListNode slow2 = head;
+	        while(slow != slow2){
+	        slow = slow.next;
+	        slow2 = slow2.next;
+	        }
+	        return slow;
+	    }
+	}
     }
 }
