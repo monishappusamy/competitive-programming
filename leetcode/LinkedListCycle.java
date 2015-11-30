f054f0cf36d4043fa391ca392ea6711086053be6
monish appusamy <monishappusamy@gmail.com>
2015-11-30 13:45:09 -0500

    Update LinkedListCycle.java

--- leetcode/LinkedListCycle.java
+++ leetcode/LinkedListCycle.java
@@ -1,4 +1,20 @@
 /**
+ * 
+ * Good Solution:
+ *  If a linked list has a cycle, there must be a node which
+ *  two different nodes point to. Taking an example:
+ *    head -> ... -> a -> b -> ... -> c -> b -> ...
+ *  Let's think about the way we reverse a linked list. The first time
+ *  that we encounter b, we make it pointing back to a, and move to
+ *  the next node (into the cycle). Now, we've processed c and move to
+ *  b again, and b is pointing to a. That is, after we process b twice,
+ *  we'll process every node before b, in the original list, and
+ *  eventually back to head.
+ *  Hence, if a linked list has a cycle, and if we reverse it, we'll
+ *  have two same head.
+ */ 
+
+/**
  * Definition for singly-linked list.
  * class ListNode {
  *     int val;
@@ -15,16 +31,16 @@ public class Solution {
             return false;
         }
         ListNode slow = head;
-		    ListNode fast = head;
-		    boolean flag = false;
-		    int i = 0;
-    		for(;;){
-  		    if(fast == null || fast.next == null)
-  			    return false;
-  			  slow = slow.next;
-  			  fast = fast.next.next;
-  			  if(slow == fast)
-  				  return true;
-    		}
+	ListNode fast = head;
+	boolean flag = false;
+	int i = 0;
+    	for(;;){
+  	    if(fast == null || fast.next == null)
+  		    return false;
+  	    slow = slow.next;
+            fast = fast.next.next;
+  	    if(slow == fast)
+  	        return true;
+    	}
     }
 }
