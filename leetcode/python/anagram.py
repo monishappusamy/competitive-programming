7b9f850417a8b4b0135eaaceee55faf9aced165a
Monish Kumar Appusamy <monishappusamy@gmail.com>
2016-04-06 15:48:28 -0400

    Create anagram.py

---
+++ leetcode/python/anagram.py
@@ -0,0 +1,25 @@
+'''
+Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+'''
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        if len(s) != len(t):
+            return False
+        
+        for letter in s:
+            if letter not in t:
+                return False
+            else:
+                t = t.replace(letter, "", 1)
+        return True
