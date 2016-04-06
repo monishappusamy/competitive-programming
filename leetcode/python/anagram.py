a0dffa21e92732b50f4838969d46304be88577aa
Monish Kumar Appusamy <monishappusamy@gmail.com>
2016-04-06 18:40:54 -0400

    Update anagram.py

--- leetcode/python/anagram.py
+++ leetcode/python/anagram.py
@@ -6,6 +6,8 @@ s = "anagram", t = "nagaram", return true.
 s = "rat", t = "car", return false.
 '''
 
+# solution 1 - 928 ms
+
 class Solution(object):
     def isAnagram(self, s, t):
         """
@@ -23,3 +25,19 @@ class Solution(object):
             else:
                 t = t.replace(letter, "", 1)
         return True
+
+
+# solution 2 - 104 ms
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        if ''.join(sorted(s)) == ''.join(sorted(t)): 
+            return True
+        else:
+            return False
