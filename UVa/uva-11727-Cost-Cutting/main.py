43859bb7797986fdc7e2d8081edbcb645aa97e81
Monish Kumar Appusamy <monishappusamy@gmail.com>
2016-04-10 10:19:13 -0400

    Create main.py

---
+++ UVa/uva-11727-Cost-Cutting/main.py
@@ -0,0 +1,18 @@
+from sys import stdin
+Game = None
+
+def solve():
+    global Game
+    Game = sorted(Game)
+    return Game[1] 
+            
+def main():
+    global Game,leng
+    inp = stdin
+    length = int(inp.readline())
+    for i in range(0, length):
+        line = inp.readline()
+        Game = [int(x) for x in line.strip().split()]
+        print ('Case ' + str(i+1) + ': ' + str(solve()))
+        
+main()
