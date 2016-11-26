0fcdce997e0da024b4d455ee3f9505327b7d7fdd
Monish Kumar Appusamy <monishappusamy@gmail.com>
2016-11-26 14:25:21 -0500

    Create Reader.java

---
+++ UVa/java/591-Box-of-Bricks/Reader.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * Created by Monish on 11/25/16.
+ */
+public class Reader {
+    static BufferedReader reader;
+    static StringTokenizer tokenizer;
+
+    /** call this method to initialize reader for InputStream */
+    static void init(InputStream input) {
+        reader = new BufferedReader(
+                new InputStreamReader(input) );
+        tokenizer = new StringTokenizer("");
+    }
+
+    /** get next word */
+    static String next() throws IOException {
+        while ( ! tokenizer.hasMoreTokens() ) {
+            tokenizer = new StringTokenizer(
+                    reader.readLine() );
+        }
+        return tokenizer.nextToken();
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt( next() );
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble( next() );
+    }
+}
